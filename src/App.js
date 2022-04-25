5c895c21fb9c9cdb7b81fbdc1c42b475f05587e4
francelys1848 <72948506+francelys1848@users.noreply.github.com>
2022-04-25 17:14:23 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello My React App</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <SlotName />
+    </div>
+  );
+}
+function SlotName() {
+  return (
+    <div className="background">
+      <div className="clear"></div>
+    </div>
+  );
+}
