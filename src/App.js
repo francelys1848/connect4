73c22a00ae9e0bf219841e0d249f626704691c09
francelys1848 <72948506+francelys1848@users.noreply.github.com>
2022-05-09 17:17:16 -0500

    completed steps 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello My React App!</h1>
       <h2>Start editing to see some magic happen!</h2>
+
+      <SlotName />
+      <SlotName />
+      <SlotName />
+      <SlotName />
+      <SlotName />
       <SlotName />
     </div>
   );
 }
 function SlotName() {
+  let [playerTurn, changePlayerTurn] = useState("red");
+  function togglePlayerTurn() {
+    if (playerTurn === "red") {
+      changePlayerTurn("yellow");
+    } else {
+      changePlayerTurn("red");
+    }
+  }
+
   return (
-    <div className="background">
-      <div className="clear"></div>
+    <div>
+      <div> {playerTurn} </div>
+      <div className="background" onClick={() => togglePlayerTurn()}>
+        <div className={`${playerTurn} clear `}></div>
+      </div>
     </div>
   );
 }
