94471b3b7ae748a37b91098c2d47507216e27267
francelys1848 <72948506+francelys1848@users.noreply.github.com>
2022-05-16 16:59:20 -0500

    column code

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import "./styles.css";
 import { useState } from "react";
 
 export default function App() {
+  let column = ["red", "yellow", "yellow", "red", "red", "yellow"];
+  console.log(column[0]);
   return (
     <div className="App">
       <h1>Hello My React App!</h1>
       <h2>Start editing to see some magic happen!</h2>
 
-      <SlotName />
-      <SlotName />
-      <SlotName />
-      <SlotName />
-      <SlotName />
-      <SlotName />
+      <SlotName token={column[5]} />
+      <SlotName token={column[4]} />
+      <SlotName token={column[3]} />
+      <SlotName token={column[2]} />
+      <SlotName token={column[1]} />
+      <SlotName token={column[0]} />
     </div>
   );
 }
-function SlotName() {
+function SlotName(props) {
   let [playerTurn, changePlayerTurn] = useState("red");
   function togglePlayerTurn() {
     if (playerTurn === "red") {
@@ -28,9 +30,9 @@ function SlotName() {
 
   return (
     <div>
-      <div> {playerTurn} </div>
+      {/* <div> {playerTurn} </div> */}
       <div className="background" onClick={() => togglePlayerTurn()}>
-        <div className={`${playerTurn} clear `}></div>
+        <div className={`${props.token} clear `}></div>
       </div>
     </div>
   );
